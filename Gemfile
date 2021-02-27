8d7b176635b2630d0505491d336a06274c512a28
Leperkhanh <jordannguyen880@gmail.com>
2021-02-27 12:58:59 -0500

    update list#show controller for nested

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'rack-cors'
 gem 'devise'
 gem 'devise-jwt'
 gem 'fast_jsonapi'
+gem 'pry', '~> 0.13.1'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,12 +1,24 @@
 class ListsController < ApplicationController
     def index
-        user = User.find_by(id: params[:id])
-        if @user 
+        user = User.find_by(id: params[:user_id])
+        if user 
             lists = user.lists 
             render json: lists
         else 
             lists = List.all
             render json: lists   
+        end
+    end
+
+    def show 
+        user = User.find_by(id: params[:user_id])
+        if user 
+            user = User.find_by(id: params[:user_id])
+            list = List.find_by(id: params[:id])
+            render json: list
+        else
+            list = List.find_by(id: params[:id])
+            render json: list       
         end
     end
     
