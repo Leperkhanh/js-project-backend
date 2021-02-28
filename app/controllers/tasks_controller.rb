3cdb3502f2063e2c9c983579c6fb7f782d84ccdb
Leperkhanh <jordannguyen880@gmail.com>
2021-02-27 22:40:37 -0500

    add tasks#index and tasks#show controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,24 @@
 class TasksController < ApplicationController
     def index 
-        tasks = Task.all 
-        render json: tasks
+        user = User.find_by(id: params[:user_id])
+        if user 
+            tasks = user.tasks 
+            render json: tasks
+        else
+            tasks = Task.all 
+            render json: tasks
+        end
+    end
+
+    def show 
+        user = User.find_by(id: params[:user_id])
+        if user 
+            task = Task.find_by(id: params[:id])
+            list = task.list
+            render json: task
+        else
+            task = Task.find_by(id: params[:id])
+            render json: task    
+        end
     end
 end
